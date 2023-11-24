a85407f77b3379acefab892f377665a4617be131
Nikita <ariastartup@gmail.com>
2023-11-24 22:32:59 +0300

    Закончил

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using MultithreadProject;
+using System.Diagnostics;
+
+int iterations = 10000;
+var array = new List<long>(iterations);
+long n = 0;
+for (long i = 0; i < iterations; i++)
+{
+    array.Add(i);
+}
+
+Console.WriteLine("Конфигурация: ОЗУ 12ГБ, процессор Intel Core i7-8750H (6 ядер)");
+Stopwatch timer = new();
+
+timer.Start();
+n = SumCalculator.SumClassic(array);
+timer.Stop();
+Console.WriteLine($"Количество итераций {iterations}, время выполнения при однопоточной работе: {timer.Elapsed}, результат {n}");
+timer.Reset();
+
+timer.Start();
+n = await SumCalculator.SumMultithreadAsync(array);
+timer.Stop();
+Console.WriteLine($"Количество итераций {iterations}, время выполнения при многопоточной работе: {timer.Elapsed}, результат {n}");
+timer.Reset();
+
+timer.Start();
+n = SumCalculator.SumLINQ(array);
+timer.Stop();
+Console.WriteLine($"Количество итераций {iterations}, время выполнения при многопоточной работе с LINQ: {timer.Elapsed}, результат {n}");
+timer.Reset();
---
+++ SumCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MultithreadProject
+{
+    public static class SumCalculator
+    {
+        public static long SumClassic(List<long> array)
+        {
+            long n = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                n += array[i];
+            }
+            return n;
+        }
+
+        public static async Task<long> SumMultithreadAsync(List<long> array)
+        {
+            Task<long> t0 = Task.Run(() => Compute(0, 6, array.Count));
+            Task<long> t1 = Task.Run(() => Compute(1, 6, array.Count));
+            Task<long> t2 = Task.Run(() => Compute(2, 6, array.Count));
+            Task<long> t3 = Task.Run(() => Compute(3, 6, array.Count));
+            Task<long> t4 = Task.Run(() => Compute(4, 6, array.Count));
+            Task<long> t5 = Task.Run(() => Compute(5, 6, array.Count));
+            long result = await t0 + await t1 + await t2 + await t3 + await t4 + await t5;
+            return result;
+        }
+
+        public static long SumLINQ(List<long> array)
+        {
+            return array.AsParallel().Sum();
+        }
+
+        public static long Compute(int start, int step, long limit)
+        {
+            long sum = 0;
+            for (long n = start; n < limit; n += step)
+                sum += n;
+            return sum;
+        }
+    }
+}
